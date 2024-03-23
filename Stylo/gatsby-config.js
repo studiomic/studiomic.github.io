0a51ef37471ae872032c0a69bae9303bb86b9447
StudioMic <jnl@studiomic.net>
2024-03-24 03:07:08 +0900

    0324

--- Stylo/gatsby-config.js
+++ Stylo/gatsby-config.js
@@ -3,7 +3,7 @@ require("dotenv").config({
 });
 
 module.exports = {
-  pathPrefix: "/Stylo",
+  pathPrefix: "/Stylo/public/",
   siteMetadata: {
     title: "Stylo de Cerise - Web Design & Web developer",
     description: "Gatsby+Contentful WebデザイナーノBLOG",
